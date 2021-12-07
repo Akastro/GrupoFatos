cfebf4b46cc2f733ae0c77eb759b0f6957ef9711
Akastro <anselmozx_ans@hotmail.com>
2021-12-07 11:31:37 -0300

    quarto commit

--- pages/index.js
+++ pages/index.js
@@ -19,6 +19,17 @@ export default function Home() {
        
       </main>
 
+      <main class="container__main">
+        
+        <aside class="container__left">...</aside>
+
+        
+        <article class="container__middle">...</article>
+
+      
+        <nav class="container__right">...</nav>
+    </main>
+
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
