5ae68611f73f33f495ee9a58b1fe2b85e63f25be
Akastro <anselmozx_ans@hotmail.com>
2021-12-27 16:08:51 -0300

    decimo segundo  commit

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,15 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
+      <meta charset="utf-8" />
+      <meta name="theme-color" content="#000333" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="#000333" />
+      <meta name="msapplication-navbutton-color" content="#000333" />
+      <meta name="msapplication-TileImage" content="https://grupofatos.com.br/contabilidade/wp-content/uploads/2017/01/cropped-logo-3-1-270x270.webp&quot;" />
+      <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1" />
+      <meta name="google-site-verification" content="mNm2aKKa259yAhiAMdt1k7Ctm1WvsS-Gh5Xycgb_osc" />
+      <meta name="description" content="Empresa de contabilidade com sede em São José dos Campos e unidade em São Paulo. Atende nacionalmente. Você merece um atendimento personalizado. Fale conosco!" />
+
         <title>Contabilidade em São Jose dos Campos</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
