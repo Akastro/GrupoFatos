4f18d0fdf51d6290524f362e305cce5cbec31c8f
Akastro <anselmozx_ans@hotmail.com>
2021-12-27 20:47:25 -0300

    vigesimo terceiro commit

--- components/Menu.js
+++ components/Menu.js
@@ -7,6 +7,18 @@ const linkStyle = {
 
 const Menu =() => (
     <div>
+        <Link>
+        <a
+          style = {linkStyle}
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Feito por{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+        </Link>
+
         <Link href="#solucoes">
         <a style = {linkStyle}>Soluções</a>     
         </Link>
