7ea33b2515fa613374fcc914d652355a82edd062
Akastro <anselmozx_ans@hotmail.com>
2021-12-27 20:32:53 -0300

    vigesimo primeiro commit

--- components/Footer.js
+++ components/Footer.js
@@ -10,6 +10,7 @@ const Footer =() => (
     <div className={styles.footer}>
         
         <a
+          style = {linkStyle}
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
