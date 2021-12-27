3786c560e125b162c2d0b9ec75599e546d543a3b
Akastro <anselmozx_ans@hotmail.com>
2021-12-27 15:51:46 -0300

    oitavo commit

--- components/Menu.js
+++ components/Menu.js
@@ -2,9 +2,26 @@ import Link from "next/link";
 
 const Menu =() => (
     <div>
-        <Link href="http://"/>
-        <a>HOME</a>
-       
+        <Link href="http://">
+        <a>Soluções</a>     
+        </Link>
+
+        <Link href="http://">
+        <a>Contabilidade</a>     
+        </Link>
+
+        <Link href="http://">
+        <a>Servilos</a>     
+        </Link>
+
+        <Link href="http://">
+        <a>Blog</a>     
+        </Link>
+
+        <Link href="http://">
+        <a>Sobre Nós</a>     
+        </Link>
+
         
         </div>
 );
