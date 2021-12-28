cec38eecbed6b02dde643ec22d4e93517dbf0987
Akastro <anselmozx_ans@hotmail.com>
2021-12-28 15:07:36 -0300

    trigesimo terceiro commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Menu from '../components/Menu';
 import Footer from '../components/Footer';
-import styles from '../components/styles/Home.module.css';
+import styles from '../styles/Home.module.css'
 
 
 
