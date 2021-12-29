70d904bc814c056ab132c64e5616c04852ec8738
Akastro <anselmozx_ans@hotmail.com>
2021-12-29 15:15:48 -0300

    62° commit

--- components/PrimeiraDobra.js
+++ components/PrimeiraDobra.js
@@ -2,7 +2,9 @@ const PrimeiraDobra = () => (
 <div className="home">
  <h1 className="frasechave">
  Você merece uma contabilidade com atendimento personalizado.
+ <p>Aqui você tem o Contador estratégico para seu negocio! </p>
  </h1>
+ 
 </div>
 
 );
