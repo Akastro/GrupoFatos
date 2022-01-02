59ae117e9ed56f3b2c5551bc7a8557274a682dca
Akastro <anselmozx_ans@hotmail.com>
2022-01-02 17:33:00 -0300

    76° commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,75 +1,46 @@
-import React from 'react';
+import * as React from 'react';
 import Head from 'next/head';
-import MyThemeProvide from 'src/components/MyThemeProvider';
-import { SettingsProvider } from 'src/contexts/SettingsContext';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { Provider } from 'next-auth/client';
-import { Router } from 'next/dist/client/router';
-import NProgress from 'nprogress';
-import 'nprogress/nprogress.css';
-
-NProgress.configure({
-  showSpinner: false,
-  trickleRate: 0.1,
-  trickleSpeed: 300,
-});
-
-Router.events.on('routeChangeStart', () => {
-  NProgress.start();
-});
-
-Router.events.on('routeChangeComplete', () => {
-  NProgress.done();
-  // scrollTo();
-});
-
-Router.events.on('routeChangeError', () => {
-  NProgress.done();
-  // scrollTo();
-});
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from '../theme'
 
 export default function MyApp(props) {
-  const { Component, pageProps } = props;
+  const{ Component, pageProps } = props;
 
   React.useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
+    const jssStyles = document.querySelector('#jss-server-side-theme');
+     if (jssStyles){
+       jssStyles.parentElement.removeChild(jssStyles);
+     }
   }, []);
 
-  return (
-    <>
+  return(
+    <div>
       <Head>
-        <title>My page</title>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
+      <meta charset="utf-8" />
+      <meta name="author" content="Anselmo Lima - Grupo Fatos" />
+      <meta name="robots" content="index, follow" />
+      <meta name="theme-color" content="#000333" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="#000333" />
+      <meta name="msapplication-navbutton-color" content="#000333" />
+      <meta name="msapplication-TileImage" content="https://grupofatos.com.br/contabilidade/wp-content/uploads/2017/01/cropped-logo-3-1-270x270.webp&quot;" />
+      <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1" />
+      <meta name="google-site-verification" content="mNm2aKKa259yAhiAMdt1k7Ctm1WvsS-Gh5Xycgb_osc" />
+      <meta name="description" content="Empresa de contabilidade com sede em São José dos Campos e unidade em São Paulo. Atende nacionalmente. Você merece um atendimento personalizado. Fale conosco!" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />" 
+
+        <title>Contabilidade em São Jose dos Campos</title>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Provider session={pageProps.session}>
-        <SettingsProvider>
-          <MyThemeProvide>
-            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-            <CssBaseline />
-            <Component {...pageProps} />
-          </MyThemeProvide>
-        </SettingsProvider>
-      </Provider>
-      <style global jsx>
-        {`
-          #nprogress {
-            position: relative;
-            z-index: 9999999;
-          }
-          #nprogress .bar {
-            background: #f44336 !important;
-            height: 3px;
-          }
-        `}
-      </style>
-    </>
-  );
+      
+      <ThemeProvider theme={theme}>
+       <CssBaseline/>
+       <Component {...pageProps}/>
+      </ThemeProvider>
+      </div>
+  )
 }
+
+
+
 
