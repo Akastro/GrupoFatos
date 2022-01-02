3ddaed704c5dff14271887bfa353a1de10f92aea
Akastro <anselmozx_ans@hotmail.com>
2022-01-02 20:59:25 -0300

    104° commit

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,24 +1,58 @@
-import { makeStyles } from "@material-ui/styles";
+import Head from 'next/head';
+import { makeStyles } from '@material-ui/core';
+import TopBar from './TopBar';
+import FooterBar from './FooterBar';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        backgroundColor: theme.backgroundColorpalette.background.dark,
-        display: 'flex',
-        height: '100vh',
-        width: '100vh',
-        overflow: 'hidden',
+  root: {
+    backgroundColor: theme.palette.background.dark,
+    display: 'flex',
+    height: '100vh',
+    overflow: 'hidden',
+    width: '100vw',
+  },
+  wrapper: {
+    display: 'flex',
+    flex: '1 1 auto',
+    overflow: 'hidden',
+    paddingTop: 64,
+    [theme.breakpoints.up('lg')]: {
+      paddingLeft: 256,
     },
-}))
+  },
+  contentContainer: {
+    display: 'flex',
+    flex: '1 1 auto',
+    overflow: 'hidden',
+  },
+  content: {
+    flex: '1 1 auto',
+    height: '100%',
+    overflow: 'auto',
+  },
+}));
 
-function Layout ({children} = {}) {
-    const classes= useStyles();
-    return (
-        <div className={classes.root}>
-            <div>Menubar</div>
-            <div>{children}</div>
-            <div>Footer</div>
+const Layout = ({ children, title = 'Tips Brazil' }) => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div className={classes.root}>
+        <TopBar />
+        <FooterBar />
+        <div className={classes.wrapper}>
+          <div className={classes.contentContainer}>
+            <div className={classes.content}>{children}</div>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Layout;
