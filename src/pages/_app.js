20424d16cc2cd64a0937950f99c5b8ed5bef401c
Akastro <anselmozx_ans@hotmail.com>
2022-01-02 17:03:40 -0300

    75° commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,29 +1,75 @@
-import * as React from 'react';
+import React from 'react';
 import Head from 'next/head';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import { CacheProvider } from '@emotion/react';
-import theme from '../theme';
-//import createEmotionCache from '../src/createEmotionCache';
+import MyThemeProvide from 'src/components/MyThemeProvider';
+import { SettingsProvider } from 'src/contexts/SettingsContext';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { Provider } from 'next-auth/client';
+import { Router } from 'next/dist/client/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 
-// Client-side cache, shared for the whole session of the user in the browser.
-const clientSideEmotionCache = createEmotionCache();
+NProgress.configure({
+  showSpinner: false,
+  trickleRate: 0.1,
+  trickleSpeed: 300,
+});
+
+Router.events.on('routeChangeStart', () => {
+  NProgress.start();
+});
+
+Router.events.on('routeChangeComplete', () => {
+  NProgress.done();
+  // scrollTo();
+});
+
+Router.events.on('routeChangeError', () => {
+  NProgress.done();
+  // scrollTo();
+});
 
 export default function MyApp(props) {
-  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  const { Component, pageProps } = props;
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
 
   return (
-    <CacheProvider value={emotionCache}>
+    <>
       <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <title>My page</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
       </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </CacheProvider>
+      <Provider session={pageProps.session}>
+        <SettingsProvider>
+          <MyThemeProvide>
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline />
+            <Component {...pageProps} />
+          </MyThemeProvide>
+        </SettingsProvider>
+      </Provider>
+      <style global jsx>
+        {`
+          #nprogress {
+            position: relative;
+            z-index: 9999999;
+          }
+          #nprogress .bar {
+            background: #f44336 !important;
+            height: 3px;
+          }
+        `}
+      </style>
+    </>
   );
 }
-
 
