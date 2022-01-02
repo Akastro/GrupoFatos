b17f086ceeb4af2a0fdf78eac04cb7ee13e5dfd0
Akastro <anselmozx_ans@hotmail.com>
2022-01-02 18:23:10 -0300

    86° commit

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,17 +1,23 @@
 import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
+import { colors } from '@material-ui/core';
 
 // Create a theme instance.
 const theme = createTheme({
    palette: {
+     background: {
+       default: colors.common.white,
+       dark:"#f4f6f8",
+       paper: colors.common.white,
+     },
     primary: {
       main: '#000333',
     },
     secondary: {
       main: '#25d366',
     },
-    error: {
-      main: red.A400,
+    text: {
+      primary: colors.blueGrey[900],
+      secondary: colors.blueGrey[600],
     },
   }, 
 });
