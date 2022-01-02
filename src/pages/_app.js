17d88fd69f25c8d4a5bd4415da3e008be074b614
Akastro <anselmozx_ans@hotmail.com>
2022-01-02 19:10:56 -0300

    96° commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -29,7 +29,7 @@ export default function MyApp(props) {
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />" 
 
         <title>Contabilidade em São Jose dos Campos</title>
-        <link rel="icon" href="/favicon.ico" />
+        
       </Head>
       
       <ThemeProvider theme={theme}>
