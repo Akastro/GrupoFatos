30ace62dba666e0b8100e951a61dfa7e99f91c7e
Akastro <anselmozx_ans@hotmail.com>
2022-01-02 19:56:53 -0300

    100° commit

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -12,7 +12,7 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 
-const pages = ['Products', 'Pricing', 'Blog'];
+const pages = ['Soluções', 'Contabilidade', 'Blog'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 const MenuBar = () => {
@@ -44,7 +44,7 @@ const MenuBar = () => {
             component="div"
             sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
           >
-            LOGO
+           <a href="/"><img src="/logo.png" alt="Grupo Fatos"></img></a>
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
