733bc067f97fadb942b7bdb6afe6871355026b45
Akastro <anselmozx_ans@hotmail.com>
2022-01-03 11:14:01 -0300

    110° commit

--- src/components/layout/FooterBar.js
+++ src/components/layout/FooterBar.js
@@ -1,4 +1,4 @@
-import Link from "components/Link";
+import Link from "../components/Link";
 import { useRouter } from "next/router";
 
 import { makeStyles } from "@material-ui/core/styles";
@@ -6,7 +6,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import { Container, Grid, Typography } from "@material-ui/core";
 
 import { routes } from "data/routes";
-import Social from "components/Social";
+import Social from "../components/Social";
 
 const useStyles = makeStyles((theme) => ({
   footer: {
