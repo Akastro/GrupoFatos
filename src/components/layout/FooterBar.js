29890919be98d34054895bb5f1fa96e2b3675ede
Akastro <anselmozx_ans@hotmail.com>
2022-01-02 21:20:11 -0300

    104° commit

--- src/components/layout/FooterBar.js
+++ src/components/layout/FooterBar.js
@@ -1,46 +1,5 @@
-import Link from "next/link";
-import styles from '../styles/Home.module.css'
-
-const linkStyle = {
-    marginRight: 15
-}
-
-
-const FooterBar =() => (
-    <div className={styles.footer}>
-        
-        <a
-          style = {linkStyle}
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Feito por{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      
-
-
-      <Link href="#solucoes">
-        <a style = {linkStyle}>Soluções</a>     
-        </Link>
-
-        <Link href="http://">
-        <a style = {linkStyle}>Contabilidade</a>     
-        </Link>
-
-        <Link href="http://">
-        <a style = {linkStyle}>Serviços</a>     
-        </Link>
-
-        <Link href="http://">
-        <a style = {linkStyle}>Blog</a>     
-        </Link>
-
-        <Link href="http://">
-        <a style = {linkStyle}>Sobre Nós</a>     
-        </Link>
-        </div>
+const Footer =() => (
+    <h1>Footer</h1>
 );
 
-export default FooterBar;
+export default Footer;
