96874d64c4c39795ca09b2ce613f9fc7425c1c47
Akastro <anselmozx_ans@hotmail.com>
2022-01-02 21:39:30 -0300

    106° commit

--- src/components/layout/TopBar.js
+++ src/components/layout/TopBar.js
@@ -10,6 +10,8 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function TopBar()  {
+    const classes = useStyles();
+    
     return (
         <AppBar ClassName={classes.root} color ='default'>
             <Toolbar>Grupo Fatos</Toolbar>
